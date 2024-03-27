53204a3700b0181b7ba5caf0cbea23a4d74351d6
PRIYA MAC <priyamac@PRIYAs-MacBook-Air.local>
2024-03-27 16:22:58 +0530

    accordin

---
+++ src/component/Accordian.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import data from "./data";
+import "./style.css";
+const Accordian = () => {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelect, setEnableMultiSelect] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+  const handleSingleSelection = (getCurrentId) => {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  };
+
+  const handleEnableMultiSelection = (getCurrentId) => {
+    let copyMultiple = [...multiple];
+    const findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId);
+    if (findIndexOfCurrentId === -1) {
+      copyMultiple.push(getCurrentId);
+    } else {
+      copyMultiple.splice(findIndexOfCurrentId, 1);
+    }
+    setMultiple(copyMultiple);
+  };
+
+  return (
+    <div className="acc-wrapper">
+      <button onClick={() => setEnableMultiSelect(!enableMultiSelect)}>
+        Enable Multi-Select
+      </button>
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div
+                className="title"
+                onClick={
+                  enableMultiSelect
+                    ? () => handleEnableMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+                {selected === dataItem.id ||
+                multiple.indexOf(dataItem.id) !== -1 ? (
+                  <div className="content">{dataItem.answer}</div>
+                ) : null}
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>No Data Found!</div>
+        )}
+      </div>
+    </div>
+  );
+};
+export default Accordian;
